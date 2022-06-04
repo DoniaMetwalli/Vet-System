65f04caa7c74731514a4ff68501a9d9060545c6b
Antonyesk601 <40663824+Antonyesk601@users.noreply.github.com>
2022-06-04 21:13:55 +0200

    SQL works lol

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,7 +1,8 @@
 module com.project217ui {
     requires javafx.controls;
     requires javafx.fxml;
-
+    requires java.sql;
     opens com.project217ui to javafx.fxml;
     exports com.project217ui;
 }
+
