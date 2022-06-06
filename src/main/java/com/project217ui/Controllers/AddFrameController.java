0e59596fde67957d0852cb753a2eb8edb8993e53
Antonyesk601 <40663824+Antonyesk601@users.noreply.github.com>
2022-06-06 05:46:50 +0200

    fixed a random warning

--- src/main/java/com/project217ui/Controllers/AddFrameController.java
+++ src/main/java/com/project217ui/Controllers/AddFrameController.java
@@ -1,8 +1,6 @@
 package com.project217ui.Controllers;
 
 import com.project217ui.Models.DoctorModel;
-import com.project217ui.Models.PetModel;
-
 public class AddFrameController {
     public static String AddPet(String ownerName,String phoneNum,String petID,String petName,String petBreed,String petAge,String visitReason,String weight, String diagnosis)
     {
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,6 +1,7 @@
 module com.project217ui {
     requires javafx.controls;
     requires javafx.fxml;
+    requires transitive javafx.graphics;
     requires java.sql;
     opens com.project217ui.Views to javafx.fxml;
     exports com.project217ui;
